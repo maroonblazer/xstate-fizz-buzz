602a8435cfc8915f979fd2cbb5232c25638bce77
Nick <nketter@gmail.com>
2019-07-27 08:16:19 -0700

    fizzbuzz working

--- index.js
+++ index.js
@@ -1,27 +1,34 @@
+// import { interpret } from 'xstate';
+
 // const xstate = require('xstate'); // we're importing xstate via index.html so we don't need this for front-end/client-side
 
-const fizzBuzzMachine = XState.Machine({
-  initial: 'digit',
-  on: {
-    increment: [
-      { cond: i => i % 3 == 0 && i % 5 == 0, target: '.fizzBuzz' },
-      { cond: i => i % 3 == 0, target: '.fizz' },
-      { cond: i => i % 5 == 0, target: '.buzz' },
-      { target: '.digit' }
-    ]
-  },
+machine = xstate.Machine({
+  initial: 'digit_fizz_buzz',
   states: {
-    digit: {
-      onEntry: 'print_digit'
-    },
-    fizz: {
-      onEntry: 'print_fizz'
-    },
-    buzz: {
-      onEntry: 'print_buzz'
-    },
-    fizzbuzz: {
-      onEntry: 'print_fizzBuzz'
+    digit_fizz_buzz: {
+      initial: 'digit',
+      on: {
+        increment: [
+          { cond: i => i % 3 == 0 && i % 5 == 0, target: '.fizzbuzz' },
+          { cond: i => i % 3 == 0, target: '.fizz' },
+          { cond: i => i % 5 == 0, target: '.buzz' },
+          { target: '.digit' }
+        ]
+      },
+      states: {
+        digit: {
+          onEntry: 'print_digit'
+        },
+        fizz: {
+          onEntry: 'print_fizz'
+        },
+        buzz: {
+          onEntry: 'print_buzz'
+        },
+        fizzbuzz: {
+          onEntry: 'print_fizzbuzz'
+        }
+      }
     }
   }
 });
@@ -33,13 +40,13 @@ const actions = {
     (document.querySelector('#digit').innerHTML += '<li>Fizz</li>'),
   print_buzz: () =>
     (document.querySelector('#digit').innerHTML += '<li>Buzz</li>'),
-  print_fizzBuzz: () =>
+  print_fizzbuzz: () =>
     (document.querySelector('#digit').innerHTML += '<li>FizzBuzz</li>')
 };
 
-let state = fizzBuzzMachine.initialState;
+let state = machine.initialState;
 
 for (let i = 1; i <= 100; i++) {
-  state = fizzBuzzMachine.transition(state, 'increment', i);
+  state = machine.transition(state, 'increment', i);
   state.actions.forEach(item => actions[item](i));
 }
